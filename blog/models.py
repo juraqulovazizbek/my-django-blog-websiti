53dd03019a79a57aa878a8a96fa0593c9f796d99
juraqulovazizbek <juraqulovazizbek85@gmail.com>
2026-01-21 14:52:29 +0500

    model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    StatusChoices = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]   
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(unique=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+    views = models.PositiveIntegerField(default=0)
+    reading_minute = models.PositiveIntegerField()
+    content_markdown = models.TextField()
+    content_html = models.TextField()
+    tg_link = models.URLField()
+    is_published = models.BooleanField(default=False)
+    status = models.CharField(choices=StatusChoices, default='draft')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
